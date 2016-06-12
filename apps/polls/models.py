8bcaa9e394397c6719a2ac2eeecb4374abe1609a
Chinmaya <patanaikchinmaya@gmail.com>
2016-06-12 20:11:27 +0530

    polls models created

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -4,22 +4,22 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 from django.db import models
 
+@python_2_unicode_compatible
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('Date Published')
 
-	@python_2_unicode_compatible
 	def __str__(self):
 		return self.question_text
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+@python_2_unicode_compatible
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
-	@python_2_unicode_compatible
 	def __str__(self):
 		return self.choice_text
