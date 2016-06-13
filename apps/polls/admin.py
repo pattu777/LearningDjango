51094ea7ff7ef03513bc97a5e61359239d811311
Chinmaya <patanaikchinmaya@gmail.com>
2016-06-13 21:41:02 +0530

    polls modified

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Question Information', {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+admin.site.register(Question, QuestionAdmin)
--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -2,6 +2,17 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # /polls/
     url(r'^$', views.index, name='index'),
+    
+    # /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    
+    # /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    
+    # /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
