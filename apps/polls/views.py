14a1d61286a2451605e42ccfdc72eca29dd5c4f7
Chinmaya <patanaikchinmaya@gmail.com>
2016-06-16 03:57:45 +0530

    new templates for home and polls app

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -7,21 +7,24 @@ from .models import Question, Choice
 
 def index(request):
 	"""Show the latest 5 polls."""
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	latest_question_list = Question.objects.order_by('-pub_date')[:10]
 	context = {
 		'latest_question_list': latest_question_list
 	}
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question' : question})
+    """Show all the available choices for the question."""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
+    """Show number of votes for a particular question."""
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
+    """Vote for a specific question."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
